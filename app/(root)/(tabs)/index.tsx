881d2d751f559a2dd6d394dc75d5cbed968178f1
Cesar <cesarnsingi@hotmail.com>
2025-11-27 12:30:12 +0100

    commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,57 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+   <SafeAreaView className="bg-white h-full">
+     <View className="px-5"> 
+     <View className="flex flex-row items-center justify-between mt-5">
+      <View className="flex flex-row items-center">
+        <Image source={images.avatar}  className="size-12 rounded-full"  />
+        <View className="flex flex-col items-start ml-2 justify-center">
+          <Text className="text-xs font-rubik text-black-100"> Bom dia</Text>
+          <Text className="text-base font-rubik-medium text-black-300">Cesar</Text>
+        </View>
+      </View>
+      <Image source={icons.bell} className="size-6" />
+      </View>
+     <Search />
+
+     <View className="my-5">
+      <View className="flex flex-row items-center justify-between">
+        <Text className="text-xl font-rubik-bold text-black-300">Em destaque</Text>
+        <TouchableOpacity>
+          <Text className="text-base font-rubik-bod 
+          text-primary-300">Ver tudo</Text>
+        </TouchableOpacity>
+      </View>
+
+       <View className="flex flex-row gap-5 mt-5">
+        <FeaturedCard/>
+        <FeaturedCard/>
+        <FeaturedCard/>
+        </View>
+      </View> 
+
+      <View className="flex flex-row items-center justify-between">
+        <Text className="text-xl font-rubik-bold text-black-300">Nossas Recomendações</Text>
+        <TouchableOpacity>
+          <Text className="text-base font-rubik-bod 
+          text-primary-300">Ver tudo</Text>
+        </TouchableOpacity>
+
+      </View>
+      <View className="flex flex-row gap-5 mt-5">
+        <Card />
+        <Card />
+      </View>
+
+     </View>
+   
+   </SafeAreaView>
+  );
+}
---
+++ app/sign-in.tsx
@@ -0,0 +1,54 @@
+import icons from '@/constants/icons';
+import images from '@/constants/images';
+
+import { login } from "@/lib/appwrite";
+import React from 'react';
+import { Alert, Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SignIn = () => {
+  const handleLogin = async () => {
+    const result = await login();
+
+    if(result) {
+      console.log('Login Success');
+    } else { 
+      Alert.alert('Error', 'Failed to login');
+    }
+  };
+
+  return (
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView contentContainerClassName='h-full'>
+        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain'/>
+        <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>
+            Bem-vindos à Kubiko
+          </Text>
+
+          <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+            Vamos conduzi-lo rumo {"\n"} 
+            <Text className='text-primary-300'>à sua casa ideal.</Text>
+          </Text>
+          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
+            Login Kubiko com google
+
+          </Text>
+          <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
+            <View className='flex flex-row items-center justify-center'>
+            <Image 
+               source={icons.google}
+               className='w-5 h-5'
+               resizeMode='contain'
+            />
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continuar com google</Text>
+            </View>
+
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default SignIn
